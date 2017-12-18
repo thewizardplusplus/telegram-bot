d75f34cc0b007458be563f757def7b3e2ca9de60
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-12-18 23:00:00 +0300

    Add a file option in the sending tool

--- tools/sender.bash
+++ tools/sender.bash
@@ -10,25 +10,29 @@ function error() {
 declare host="localhost"
 declare -i port=4000
 declare text=""
-while getopts "hH:P:t:" option; do
+declare file=""
+while getopts "hH:P:t:f:" option; do
 	case "$option" in
 		h)
 			declare -r script_name="$(basename "$0")"
 			echo "Usage:"
 			echo "  $script_name -h"
 			echo "  $script_name [-H HOST] [-P PORT] -t TEXT"
+			echo "  $script_name [-H HOST] [-P PORT] -f PATH"
 			echo
 			echo "Options:"
 			echo "  -h       - show this help message;"
 			echo "  -H HOST  - set a host name (default: localhost);"
 			echo "  -P PORT  - set a port number (default: 4000);"
-			echo "  -t TEXT  - set a message text."
+			echo "  -t TEXT  - set a message text;"
+			echo "  -f PATH  - set a path to a message photo."
 
 			exit 0
 			;;
 		H) host="$OPTARG";;
 		P) port="$OPTARG";;
 		t) text="$OPTARG";;
+		f) file="$OPTARG";;
 		?) exit 1;;
 	esac
 done
