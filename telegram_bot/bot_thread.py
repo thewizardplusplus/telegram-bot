d0fc95bb63c46f02121e69113712dc7becbad17f
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-01-21 23:00:00 +0300

    Add the _init_bot_polling() function

--- telegram_bot/bot_thread.py
+++ telegram_bot/bot_thread.py
@@ -1,5 +1,6 @@
 import threading
 import functools
+import time
 
 import termcolor
 
@@ -7,34 +8,37 @@ from . import bot
 from . import db
 from . import logger
 from . import utils
+from . import env
 
 class BotThread(threading.Thread):
     def __init__(self, bot_client):
         super().__init__()
         self._bot_client = bot_client
 
-    @utils.catch_exceptions
     def run(self):
-        logger.get_logger().info(
-            'run a %s bot polling',
-            termcolor.colored('Telegram', 'magenta'),
-        )
-
-        db_connection = db.init_db()
-        self._bot_client.callback_query_handler(
-            func=lambda call: call.data in ['accept', 'reject'],
-        )(functools.partial(
-            _handle_button_click,
-            self._bot_client,
-            db_connection,
-        ))
-
-        self._bot_client.polling(none_stop=True)
+        restart_delay = int(env.get_env('RESTART_DELAY', 1000)) / 1000
+        while True:
+            _init_bot_polling(self._bot_client)
+            time.sleep(restart_delay)
 
 def init_bot_thread(bot_client):
     thread = BotThread(bot_client)
     thread.setDaemon(True)
     thread.start()
+
+@utils.catch_exceptions
+def _init_bot_polling(bot_client):
+    logger.get_logger().info(
+        'run a %s bot polling',
+        termcolor.colored('Telegram', 'magenta'),
+    )
+
+    db_connection = db.init_db()
+    bot_client.callback_query_handler(
+        func=lambda call: call.data in ['accept', 'reject'],
+    )(functools.partial(_handle_button_click, bot_client, db_connection))
+
+    bot_client.polling(none_stop=True)
 
 @utils.catch_exceptions
 def _handle_button_click(bot_client, db_connection, call):
